53097c6f7205ef0abddf55764e5e9afc1d01c766
faria-r <fariacse11@gmail.com>
2024-05-24 15:50:24 +0600

    Setup MongoDB for storing DATA

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxeycza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+//testing server msg
+app.get("/", async (req, res) => {
+  res.send("Bake server is running");
+});
+
+app.listen(port, () => console.log(`Bake server is running at ${port}`));
